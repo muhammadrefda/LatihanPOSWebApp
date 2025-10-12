ca8f7dc8193fbea8f6b44be63ccee89bc0902a18
muhammad_refda <muhammadrefda@gmail.com>
2025-10-12 14:44:48 +0700

    feat: Create Product model with relationships to Category and Tag

---
+++ PointOfSalesWebApp/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace PointOfSalesWebApp.Models
+{
+    public class Product : BaseEntity
+    {
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public bool Active { get; set; } = true;
+
+        // 1 to m
+        public long CategoryId { get; set; }
+        public Category Category { get; set; }
+
+
+        // m to m
+        public ICollection<Tag> Tags { get; set; } = new List<Tag>();
+    }
+}
