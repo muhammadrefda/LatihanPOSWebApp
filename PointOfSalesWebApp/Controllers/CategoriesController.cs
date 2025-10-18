a0878be042bb057d619b1238e9a05328e31b4274
Narancia <alhabsyir@gmail.com>
2025-10-19 00:10:54 +0700

    feat: Add edit functionality and validation Active

--- PointOfSalesWebApp/Controllers/CategoriesController.cs
+++ PointOfSalesWebApp/Controllers/CategoriesController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using PointOfSalesWebApp.Data;
+using PointOfSalesWebApp.Models;
 
 namespace PointOfSalesWebApp.Controllers
 {
@@ -18,6 +20,74 @@ namespace PointOfSalesWebApp.Controllers
             return View(categories);
         }
 
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(category);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(category);
+        }
+
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == 0) return NotFound();
+
+            var category = await _context.Categories.FindAsync(id);
+            if (category == null ) return NotFound();
+
+            ViewBag.ActiveList = new List<SelectListItem> {
+    new SelectListItem { Value = "true",  Text = "Aktif",    Selected = category.Active },
+    new SelectListItem { Value = "false", Text = "Nonaktif", Selected = !category.Active }
+};
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> Edit(long id, Category category)
+        {
+            if ( id != category.Id) return NotFound();
+
+            var inUse = await _context.Products.AnyAsync(p => p.CategoryId == category.Id);
+
+            if(!category.Active && inUse)
+            {
+                ModelState.AddModelError(nameof(category.Active), "Masih ada produk yang menggunakan kategori ini, mohon ubah kategori/hapus produk terlebih dahulu");
+            }
+
+            if(ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(category);
+                    await _context.SaveChangesAsync();
+                }
+                catch(DbUpdateConcurrencyException)
+                {
+                    if (!_context.Categories.Any(e => e.Id == category.Id)) return NotFound();
+                    else throw;
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewBag.ActiveList = new List<SelectListItem> {
+    new SelectListItem { Value = "true",  Text = "Aktif",    Selected = category.Active },
+    new SelectListItem { Value = "false", Text = "Nonaktif", Selected = !category.Active }
+};
+            return View(category) ;
+        }
 
     }
 }
--- PointOfSalesWebApp/Models/Category.cs
+++ PointOfSalesWebApp/Models/Category.cs
@@ -4,10 +4,11 @@ namespace PointOfSalesWebApp.Models
 {
     public class Category : BaseEntity
     {
+        [Display(Name = "Nama Kategori")]
         public string CategoryName { get; set; }
+        [Display(Name = "Deskripsi")]
         public string? Description { get; set; }
-        
-            
+        [Display(Name ="Status")]
         public bool Active { get; set; } = true;
     }
 }
