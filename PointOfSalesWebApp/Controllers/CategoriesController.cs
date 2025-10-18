156897657e39c44617b11c15cba0db602d8e5f94
Narancia <alhabsyir@gmail.com>
2025-10-19 00:54:04 +0700

    feat: Add delete functionality.

--- PointOfSalesWebApp/Controllers/CategoriesController.cs
+++ PointOfSalesWebApp/Controllers/CategoriesController.cs
@@ -48,9 +48,9 @@ namespace PointOfSalesWebApp.Controllers
             if (category == null ) return NotFound();
 
             ViewBag.ActiveList = new List<SelectListItem> {
-    new SelectListItem { Value = "true",  Text = "Aktif",    Selected = category.Active },
-    new SelectListItem { Value = "false", Text = "Nonaktif", Selected = !category.Active }
-};
+                new SelectListItem { Value = "true",  Text = "Aktif",    Selected = category.Active },
+                new SelectListItem { Value = "false", Text = "Nonaktif", Selected = !category.Active }
+            };
             return View(category);
         }
 
@@ -83,10 +83,45 @@ namespace PointOfSalesWebApp.Controllers
                 return RedirectToAction(nameof(Index));
             }
             ViewBag.ActiveList = new List<SelectListItem> {
-    new SelectListItem { Value = "true",  Text = "Aktif",    Selected = category.Active },
-    new SelectListItem { Value = "false", Text = "Nonaktif", Selected = !category.Active }
-};
+                new SelectListItem { Value = "true",  Text = "Aktif",    Selected = category.Active },
+                new SelectListItem { Value = "false", Text = "Nonaktif", Selected = !category.Active }
+            };
             return View(category) ;
+        }
+
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == 0) return NotFound();
+
+            var category = await _context.Categories.FirstOrDefaultAsync(m=>m.Id == id);
+
+            if (category == null) return NotFound();
+
+            return View(category);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> DeleteConfirmed(long? id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+
+            if (category == null) return NotFound();
+
+
+            if (ModelState.IsValid && !category.Active)
+            {
+                _context.Categories.Remove(category);
+                await _context.SaveChangesAsync();
+            }
+            else
+            {
+                ModelState.AddModelError(nameof(category.Active), "Kategori masih aktif! harap nonaktifkan dulu sebelum menghapus!");
+                return View(category);
+            }
+
+            return RedirectToAction(nameof(Index)); 
         }
 
     }
