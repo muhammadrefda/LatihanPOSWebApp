089c23951dec297f8392bf9c02b19757b341c4bf
Narancia <alhabsyir@gmail.com>
2025-10-18 20:09:25 +0700

    feat: Create and style product list view

--- PointOfSalesWebApp/Controllers/ProductsController.cs
+++ PointOfSalesWebApp/Controllers/ProductsController.cs
@@ -4,19 +4,17 @@ using PointOfSalesWebApp.Data;
 
 namespace PointOfSalesWebApp.Controllers
 {
-    public class ProductController : Controller
+    public class ProductsController : Controller
     {
-
         public readonly ApplicationDbContext _context;
-        public ProductController(ApplicationDbContext context)
+        public ProductsController(ApplicationDbContext context)
         {
             _context = context;
         }
         public async Task<IActionResult> Index()
         {
             var products = await _context.Products.Include(p => p.Category).ToListAsync();
-
-            return View();
+            return View(products);
         }
     }
 }
