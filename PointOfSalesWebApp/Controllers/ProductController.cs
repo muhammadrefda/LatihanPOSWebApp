40f959f367176dd23729836e42cff3eae369b78f
Narancia <alhabsyir@gmail.com>
2025-10-18 19:44:44 +0700

    feat: Create ProductsController and implement Index action to fetch data

---
+++ PointOfSalesWebApp/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PointOfSalesWebApp.Data;
+
+namespace PointOfSalesWebApp.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        public readonly ApplicationDbContext _context;
+        public ProductController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var products = await _context.Products.Include(p => p.Category).ToListAsync();
+
+            return View();
+        }
+    }
+}
