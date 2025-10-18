1d6af2ae58dd81469cf61a6c0ad1f2ca081ada71
Narancia <alhabsyir@gmail.com>
2025-10-18 23:06:59 +0700

    feat: Add Categories Controller and View Index

---
+++ PointOfSalesWebApp/Controllers/CategoriesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PointOfSalesWebApp.Data;
+
+namespace PointOfSalesWebApp.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public readonly ApplicationDbContext _context;
+
+        public CategoriesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var categories = _context.Categories.ToList();
+            return View(categories);
+        }
+
+
+    }
+}
--- PointOfSalesWebApp/Models/Category.cs
+++ PointOfSalesWebApp/Models/Category.cs
@@ -1,9 +1,13 @@
-﻿namespace PointOfSalesWebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PointOfSalesWebApp.Models
 {
     public class Category : BaseEntity
     {
         public string CategoryName { get; set; }
         public string? Description { get; set; }
+        
+            
         public bool Active { get; set; } = true;
     }
 }
